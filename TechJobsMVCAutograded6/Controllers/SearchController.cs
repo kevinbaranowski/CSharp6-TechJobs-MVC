1f696eba17e7e0ce802e5a07b24b9a47cea0724d
Kevin Baranowski <kevindbaranowski@gmail.com>
2024-06-12 10:24:12 -0500

    assignment 3 completion

--- TechJobsMVCAutograded6/Controllers/SearchController.cs
+++ TechJobsMVCAutograded6/Controllers/SearchController.cs
@@ -18,5 +18,28 @@ public class SearchController : Controller
     }
 
     // TODO #3 - Create an action method to process a search request and render the updated search views.
+
+    public IActionResult Results(string searchType, string searchTerm)
+    {
+        List<Job> jobs;
+        if (searchType == "all")
+        {
+            if (string.IsNullOrEmpty(searchTerm))
+            {
+                jobs = JobData.FindAll();
+            }
+            else 
+            {
+                jobs = JobData.FindByColumnAndValue(searchType, searchTerm);
+            } 
+        }
+        else
+        {
+            jobs = JobData.FindByColumnAndValue(searchType, searchTerm);
+        }
+        ViewBag.columns = ListController.ColumnChoices;
+        ViewBag.jobs = jobs;
+        return View("Index");
+    }
 }
 
